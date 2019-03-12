c736a614c9068a43dc4ae53aca1fa2f84e642284
Student <student@c-20-4.local>
2019-03-12 15:39:00 +0300

    PushButton

---
+++ Water/PushButton.swift
@@ -0,0 +1,21 @@
+//
+//  PushButton.swift
+//  Water
+//
+//  Created by Student on 12/03/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class PushButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
